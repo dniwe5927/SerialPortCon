aae333692947dedaf035ca5cbfdf5a639c236c03
dniwe5927 <iyangazov5@gmail.com>
2022-08-21 23:34:08 +0300

    modified:   SerailPortCon/Form1.h 	modified:   SerailPortCon/Form1.resX 	SerailPortCon.sdf 	SerailPortCon.suo 	SerailPortCon/Debug/ 	SerailPortCon/SerailPortCon.vcxproj.user

--- SerailPortCon/Form1.h
+++ SerailPortCon/Form1.h
@@ -8,7 +8,7 @@ namespace SerailPortCon {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
-
+	using namespace System::IO::Ports;
 	/// <summary>
 	/// Summary for Form1
 	/// </summary>
@@ -18,6 +18,7 @@ namespace SerailPortCon {
 		Form1(void)
 		{
 			InitializeComponent();
+			findPorts();
 			//
 			//TODO: Add the constructor code here
 			//
@@ -34,12 +35,29 @@ namespace SerailPortCon {
 				delete components;
 			}
 		}
+	private: System::IO::Ports::SerialPort^  serialPort1;
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::ComboBox^  comboBox2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::StatusStrip^  statusStrip1;
+	private: System::Windows::Forms::ProgressBar^  progressBar1;
+
+	protected: 
+
+
+	private: System::ComponentModel::IContainer^  components;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,13 +66,148 @@ namespace SerailPortCon {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->components = (gcnew System::ComponentModel::Container());
+			this->serialPort1 = (gcnew System::IO::Ports::SerialPort(this->components));
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->fileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->statusStrip1 = (gcnew System::Windows::Forms::StatusStrip());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
+			this->menuStrip1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->fileToolStripMenuItem});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(965, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this->fileToolStripMenuItem->Name = L"fileToolStripMenuItem";
+			this->fileToolStripMenuItem->Size = System::Drawing::Size(35, 20);
+			this->fileToolStripMenuItem->Text = L"file";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(326, 3);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(75, 21);
+			this->comboBox1->TabIndex = 1;
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(6) {L"4800", L"9600", L"19200", L"38400", L"57600", 
+				L"115200"});
+			this->comboBox2->Location = System::Drawing::Point(444, 3);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(75, 21);
+			this->comboBox2->TabIndex = 2;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(293, 6);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(31, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"COM";
+			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(404, 6);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(38, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Speed";
+			this->label2->Click += gcnew System::EventHandler(this, &Form1::label2_Click);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(534, 1);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(38, 23);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"start";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(578, 1);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(38, 23);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"stop";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// statusStrip1
+			// 
+			this->statusStrip1->Location = System::Drawing::Point(0, 370);
+			this->statusStrip1->Name = L"statusStrip1";
+			this->statusStrip1->Size = System::Drawing::Size(965, 22);
+			this->statusStrip1->TabIndex = 8;
+			this->statusStrip1->Text = L"statusStrip1";
+			// 
+			// progressBar1
+			// 
+			this->progressBar1->Location = System::Drawing::Point(853, 370);
+			this->progressBar1->Name = L"progressBar1";
+			this->progressBar1->Size = System::Drawing::Size(100, 23);
+			this->progressBar1->TabIndex = 9;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(965, 392);
+			this->Controls->Add(this->progressBar1);
+			this->Controls->Add(this->statusStrip1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->menuStrip1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+		//find serial port
+		private: void findPorts(void){
+			//get array ports  name
+			array<Object^>^ objectArray = SerialPort::GetPortNames();
+			this->comboBox1->Items->AddRange( objectArray );
+		}
+	private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void toolStripContainer1_TopToolStripPanel_Click(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+			 }
+private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+};
 }
 
